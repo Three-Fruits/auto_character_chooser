32bb8beab173f15881ccc5b073daeeecf01224a9
HerrMarmelade <alinagitural@gmail.com>
2022-08-23 21:50:07 +0400

    images
    nese elemisem

--- lib/themes/images.dart
+++ lib/themes/images.dart
@@ -8,20 +8,29 @@ class MyImages {
 
   //Valorant
   static const String valorantBackground =
-      'images/Valorant/valorant_background.jpg';
+      'images/Valorant/valorant_background.png';
   static const String valorantPistol =
       'images/Valorant/Classic_prime_VALORANT.png';
   static const String valorantPistolDefault = 'images/Valorant/displayicon.png';
-  static const String valorantBanner = 'images/Valorant/valorantBanner.webp';
+  static const String valorantBanner = 'images/Valorant/valorant_banner.png';
   static const String valorantLogo = 'images/Valorant/valorant_logo.png';
+  static const String agenticon = 'images/Valorant/agenticon.png';
 
+  //TF2
   static const String tf2background =
       'images/Team_Fortress_2/tf2background.png';
-  static const String tf2logo =
-      'images/Team_Fortress_2/tf2 logo.png';
-  static const String tf2backbanner = 'images/Team_Fortress_2/YNQh421.jpg';
+  static const String tf2logo = 'images/Team_Fortress_2/tf2_logo.png';
+  static const String tf2backbanner = 'images/Team_Fortress_2/tf2fun.png';
+  static const String tf2icon = 'images/Team_Fortress_2/classicon.png';
+  static const String tf2group = 'images/Team_Fortress_2/tf2group.png';
 
-  static const String rainbowbackBanner = 'images/Rainbow_Six_Siege/rainbow-six-siege-operators.jpg';
+  //R6S
+  static const String rainbowbackBanner =
+      'images/Rainbow_Six_Siege/r6s_operators.png';
+  static const String r6s2 = 'images/Rainbow_Six_Siege/r6s.png';
 
-  static const String apexbackBanner = 'images/Apex_Legends/Apex_Legends.jpg';
+  //Apex
+  static const String apex1 = 'images/Apex_Legends/apex1.png';
+  static const String apexbackBanner = 'images/Apex_Legends/apex2.png';
+  static const String apex3 = 'images/Apex_Legends/apex3.png';
 }
